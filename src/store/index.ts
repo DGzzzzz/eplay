bd905620cb6bdb22825f95c5873d4b7bbca5a745
DGzzzzz <douglaswinter@outlook.com>
2024-06-25 20:18:30 -0300

    feat: criado service api para comunicação entre app e serviço

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
+import api from '../services/api'
+
+import cart from './reducers/cart'
+import { buildGetDefaultMiddleware } from '@reduxjs/toolkit/dist/getDefaultMiddleware'
 
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    cart: cart,
+    [api.reducerPath]: api.reducer
+  },
+  middleware: (buildGetDefaultMiddleware) =>
+    buildGetDefaultMiddleware().concat(api.middleware)
 })
 
 export type RootReducer = ReturnType<typeof store.getState>
