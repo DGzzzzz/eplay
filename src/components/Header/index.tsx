38c11d6aeb48b85382a508d4c777ea95b7133a69
DGzzzzz <douglaswinter@outlook.com>
2024-07-06 08:39:20 -0300

    feat: contrução menu responsivo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,26 @@
 import { Link } from 'react-router-dom'
-import { HeaderBar, Links, LinkItem, CartButton } from './styles'
+import {
+  HeaderBar,
+  Links,
+  LinkItem,
+  CartButton,
+  HeaderRow,
+  NavMobile,
+  Hamburguer
+} from './styles'
 import logo from '../../assets/images/logo.svg'
 import carrinho from '../../assets/images/carrinho.svg'
 
 import { open } from '../../store/reducers/cart'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootReducer } from '../../store'
+import { HashLink } from 'react-router-hash-link'
+import { useState } from 'react'
 
 const Header = () => {
   const dispatch = useDispatch()
   const { items } = useSelector((state: RootReducer) => state.cart)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   const openCart = () => {
     dispatch(open())
@@ -17,28 +28,84 @@ const Header = () => {
 
   return (
     <HeaderBar>
-      <div>
-        <Link to="/">
-          <img src={logo} alt="Eplay" />
-        </Link>
-        <nav>
-          <Links>
-            <LinkItem>
-              <Link to="/categorias">Categorias</Link>
-            </LinkItem>
-            <LinkItem>
-              <a href="#">Novidades</a>
-            </LinkItem>
-            <LinkItem>
-              <a href="#">Promoções</a>
-            </LinkItem>
-          </Links>
-        </nav>
-      </div>
-      <CartButton onClick={openCart}>
-        {items.length} - Produto(s)
-        <img src={carrinho} alt="Carrinho" />
-      </CartButton>
+      <HeaderRow>
+        <div>
+          <Hamburguer onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <span />
+            <span />
+            <span />
+          </Hamburguer>
+          <Link to="/">
+            <h1>
+              <img src={logo} alt="EPLAY" />
+            </h1>
+          </Link>
+          <nav>
+            <Links>
+              <LinkItem>
+                <Link
+                  title="Clique aqui para acessar a página de categorias"
+                  to="/categories"
+                >
+                  Categorias
+                </Link>
+              </LinkItem>
+              <LinkItem>
+                <HashLink
+                  title="Clique aqui para acessar a seção de em breve"
+                  to="/#coming-soon"
+                >
+                  Em breve
+                </HashLink>
+              </LinkItem>
+              <LinkItem>
+                <HashLink
+                  title="Clique aqui para acessar a seção de promoções"
+                  to="/#on-sale"
+                >
+                  Promoções
+                </HashLink>
+              </LinkItem>
+            </Links>
+          </nav>
+        </div>
+        <CartButton role="button" onClick={openCart}>
+          {items.length}
+          <span> - produto(s)</span>
+          <img src={carrinho} alt="Carrinho" />
+        </CartButton>
+      </HeaderRow>
+      <NavMobile className={isMenuOpen ? 'is-open' : ''}>
+        <Links>
+          <LinkItem>
+            <Link
+              title="Clique aqui para acessar a página de categorias"
+              to="/categories"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Categorias
+            </Link>
+          </LinkItem>
+          <LinkItem>
+            <HashLink
+              title="Clique aqui para acessar a seção de em breve"
+              to="/#coming-soon"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Em breve
+            </HashLink>
+          </LinkItem>
+          <LinkItem>
+            <HashLink
+              title="Clique aqui para acessar a seção de promoções"
+              to="/#on-sale"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Promoções
+            </HashLink>
+          </LinkItem>
+        </Links>
+      </NavMobile>
     </HeaderBar>
   )
 }
