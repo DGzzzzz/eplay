9290072df49aa9b76f0d6792dc3a0533b9724de7
DGzzzzz <douglaswinter@outlook.com>
2024-07-07 21:22:52 -0300

    fix: ajustado Loader da home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,29 +7,25 @@ const Home = () => {
   const { data: onSaleGames, isLoading: isLoadingSale } = useGetOnSaleQuery()
   const { data: soonGames, isLoading: isLoadingSoon } = useGetSoonQuery()
 
-  if (onSaleGames && soonGames) {
-    return (
-      <>
-        <Banner />
-        <ProductsList
-          games={onSaleGames}
-          title="Promoções"
-          background="gray"
-          id="on-sale"
-          isLoading={isLoadingSale}
-        />
-        <ProductsList
-          games={soonGames}
-          title="Em breve"
-          background="black"
-          id="coming-soon"
-          isLoading={isLoadingSoon}
-        />
-      </>
-    )
-  }
-
-  return <h4>Carregando...</h4>
+  return (
+    <>
+      <Banner />
+      <ProductsList
+        games={onSaleGames}
+        title="Promoções"
+        background="gray"
+        id="on-sale"
+        isLoading={isLoadingSale}
+      />
+      <ProductsList
+        games={soonGames}
+        title="Em breve"
+        background="black"
+        id="coming-soon"
+        isLoading={isLoadingSoon}
+      />
+    </>
+  )
 }
 
 export default Home
